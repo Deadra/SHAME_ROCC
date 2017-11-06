bcb49ed5537d2a1e766ff40c0932f5a6af8e5a56
aizenbit <aizenbit@gmail.com>
2017-11-06 18:20:06 +0300

    implemented dumb respawn system

--- Assets/Scripts/Services/SceneManager.cs
+++ Assets/Scripts/Services/SceneManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -8,62 +9,67 @@ public class SceneManager : NetworkBehaviour
     private bool IAmUseless = false;
 
     [SerializeField] NetManager netManager;
+    [SerializeField] float respawnTime = 10f;
     [Header("Enemies Spawn Points")]
     List<GameObject> enemyPrefabs;
     [SerializeField] List<Transform> spawnPoints;
-    
+
+    List<GameObject> spawnedEnemies;
+
     void Awake()
     {
         if (instance == null)
             instance = this;
         else
         {
-            Debug.LogWarningFormat("Multiple instances of {0} (singleton) on the scene (objects {1}, {2})! Exterminate!!!1", 
+            Debug.LogWarningFormat("Multiple instances of {0} (singleton) on the scene (objects {1}, {2})! Exterminate!!!1",
                                     this.GetType(), instance.gameObject.name, gameObject.name);
             IAmUseless = true;
             Destroy(this);
         }
         enemyPrefabs = new List<GameObject>();
-
-        //if (!FindObjectOfType<NetManager>())
-        //    Debug.LogErrorFormat("Can't find NetManager on the scene! Prefabs will not be spawned!");
         enemyPrefabs.AddRange(netManager.enemyPrefabs);
-        //netManager.AddSpawnablePrefabs(enemyPrefabs);
+
+        spawnedEnemies = new List<GameObject>();
     }
-    
+
     void Start()
     {
-        CmdSpawnEnemies();
+        if (isServer)
+            CmdSpawnEnemies();
+
+        if (isServer)
+            StartCoroutine(RespawnEnemies());
     }
 
     [Command]
     void CmdSpawnEnemies()
     {
-        foreach(var spawnPoint in spawnPoints)
-            foreach(var enemyPrefab in enemyPrefabs)
+        foreach (var spawnPoint in spawnPoints)
+            foreach (var enemyPrefab in enemyPrefabs)
             {
                 var enemy = Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
                 enemy.GetComponent<FrogAI>().patrolingCenter = GameObject.FindWithTag("FrogPatrolCenter").transform;
                 NetworkServer.Spawn(enemy);
-                //RpcSpawnEnemy(enemy);
-                //enemy.GetComponentInChildren<BaseAI>().enabled = true;
-                //enemy.GetComponentInChildren<BaseEntity>().enabled = true;
+                spawnedEnemies.Add(enemy);
             }
-        
-    }
 
-    [ClientRpc]
-    void RpcSpawnEnemy(GameObject enemy)
-    {
-        if (Network.isServer)
-            return;
-
-        enemy.GetComponentInChildren<BaseAI>().enabled = false;
-        enemy.GetComponentInChildren<BaseEntity>().enabled = false;
     }
     public void OnDestroy()
     {
         if (!IAmUseless)
             instance = null;
+    }
+
+    private IEnumerator RespawnEnemies()
+    {
+        while (true)
+        { 
+            yield return new WaitForSeconds(respawnTime);
+            spawnedEnemies.RemoveAll((GameObject enemy) => { return enemy == null; });
+
+            if (spawnedEnemies.Count == 0)
+                CmdSpawnEnemies();
+        }
     }
 }
