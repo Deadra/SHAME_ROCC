2e69b53c5047048cf76325c934f12e77091e394e
labgraphsuai <labgraphsuai@gmail.com>
2017-12-05 14:34:03 +0300

    vive player fixed

---
+++ Assets/Scripts/Misc/MatchTrasform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchTrasform : MonoBehaviour {
+
+    [SerializeField] Transform objectToMatch;
+
+    void Start()
+    {
+
+    }
+
+    void Update () 
+    {
+        this.transform.rotation = objectToMatch.transform.rotation;
+        this.transform.position = objectToMatch.transform.position;
+		
+	}
+}
