c58093096145f58dbcade9611fcb5b042f1d1e93
aizenbit <aizenbit@gmail.com>
2018-02-06 18:41:07 +0300

    moved the weapon logic to the BasePlayer; added several gun slots for one player; added a machine gun to the buggy

---
+++ Assets/Scripts/Inputs/XDInput.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Vehicles.Car;
+
+/// <summary>
+/// В зависимости от ввода игрока этот класс вызывает функции управления игровым персонажем 
+/// </summary>
+[RequireComponent(typeof(XDPlayer), typeof(CarController))]
+public class XDInput : NetworkBehaviour
+{
+    XDPlayer player;
+    CarController mover;
+
+    void Start()
+    {
+        player = GetComponent<XDPlayer>();
+        mover = GetComponent<CarController>();
+    }
+
+    void FixedUpdate()
+    {
+        float h = CrossPlatformInputManager.GetAxis("XDHorizontal");
+        float v = CrossPlatformInputManager.GetAxis("XDVertical");
+        float b = CrossPlatformInputManager.GetAxis("XDBrake");
+        float handbrake = CrossPlatformInputManager.GetAxis("Jump");
+
+        mover.Move(h, v, b, handbrake);
+
+        if (Input.GetButton("Fire1"))
+            player.FireGun();
+
+        if (Input.GetButtonDown("Fire3"))
+            player.SwitchGun();
+    }
+}
--- Assets/Scripts/Players/BasePlayer.cs
+++ Assets/Scripts/Players/BasePlayer.cs
@@ -1,12 +1,19 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using System.Collections.Generic;
 
 /// <summary>
 /// Основной класс для игровых персонажей. В момент сметри возрождается на стартовой позиции,
 /// обладает интерфейсом: миникартой и полосой здоровья
 /// </summary>
-public class BasePlayer : BaseEntity {
+[RequireComponent(typeof(SpawnManager))]
+public class BasePlayer : BaseEntity
+{
+    [SerializeField] private List<BaseWeapon> weapons;
+    private int currentWeaponIndex = 0;
+    [SerializeField] private List<Transform> weaponSlots;
+    private Dictionary<Transform, BaseWeapon> currentWeapons;
 
     [SerializeField] private Slider healthBar;
     [SerializeField] private GameObject minimapCamera;
@@ -16,6 +23,11 @@ public class BasePlayer : BaseEntity {
     public override void Start()
     {
         base.Start();
+
+        currentWeapons = new Dictionary<Transform, BaseWeapon>();
+        foreach (var slot in weaponSlots)
+            currentWeapons.Add(slot, null);
+
         startingPosition = transform.position;
         startingRotation = transform.rotation;
     }
@@ -36,7 +48,6 @@ public class BasePlayer : BaseEntity {
             minimapCamera.transform.position = pos;
             minimapCamera.transform.rotation = Quaternion.LookRotation(Vector3.down, transform.forward);
         }
-
     }
 
     [ClientRpc]
@@ -47,4 +58,78 @@ public class BasePlayer : BaseEntity {
         transform.rotation = startingRotation;
     }
 
+    public void FireGun()
+    {
+        foreach(var currentWeapon in currentWeapons)
+            currentWeapon.Value.Fire();
+    }
+
+    public void SwitchGun()
+    {
+        currentWeaponIndex += 1;
+        currentWeaponIndex = currentWeaponIndex % weapons.Count;
+
+        spawnGunsInAllSlots(currentWeaponIndex);
+    }
+
+    /// <summary>
+    /// Спаунит пушку сразу во всех слотах
+    /// </summary>
+    /// <param name="weaponIndex"></param>
+    public void spawnGunsInAllSlots(int weaponIndex = 0)
+    {
+        if (weaponIndex >= weapons.Count)
+            return;
+
+        for (int i = 0; i < weaponSlots.Count; i++)
+            CmdSpawnGun(weaponIndex, i);
+    }
+
+    /// <summary>
+    /// Спаунит пушку в указанном слоте и отправляет информацию об этом клиенту. Выполняется на стороне сервера.
+    /// </summary>
+    [Command]
+    void CmdSpawnGun(int weaponIndex, int slotIndex)
+    {
+        var slot = weaponSlots[slotIndex];
+        if(currentWeapons[slot] != null)
+            Destroy(currentWeapons[slot].gameObject);
+        currentWeapons[slot] = null;
+
+        currentWeaponIndex = weaponIndex;
+        var weapon = Instantiate(weapons[currentWeaponIndex], slot);
+        weapon.parentNetID = this.netId;
+        weapon.slotIndex = slotIndex;
+
+        NetworkServer.Spawn(weapon.gameObject);
+        RpcSpawnGun(weapon.gameObject, weaponIndex, slotIndex);
+        SpawnGunSetup(weapon);
+    }
+
+    /// <summary>
+    /// Обновляет пушку в соответствии с инфой, пришедшей от сервера. Выполняется на стороне клиента.
+    /// </summary>
+    [ClientRpc]
+    void RpcSpawnGun(GameObject weapon, int weaponIndex, int slotIndex)
+    {
+        currentWeaponIndex = weaponIndex;
+        var slot = weaponSlots[slotIndex];
+        currentWeapons[slot] = weapon.GetComponent<BaseWeapon>();
+        SpawnGunSetup(weapon.GetComponent<BaseWeapon>());
+    }
+
+    /// <summary>
+    /// Настраивает параметры пушки, помещает её в руку игрока
+    /// </summary>
+    private void SpawnGunSetup(BaseWeapon weapon)
+    {
+        weapon.Team = Team;
+        weapon.Holder = this;
+        weapon.transform.parent = weaponSlots[weapon.slotIndex];
+    }
+
+    public Transform getWeaponSlot(int index)
+    {
+        return weaponSlots[index];
+    }
 }
